1a88a92ac534bb51fa6347e215312252f9b7fdcb
Sabrina Button <sabrinarosebutton@gmail.com>
2023-01-30 14:54:38 -0500

    created baseline landing page

--- ventus-ui/src/App.js
+++ ventus-ui/src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Ventus Robotics</h1>
+        <img src={logo}></img>
         <p>Coming soon...</p>
       </header>
     </div>
